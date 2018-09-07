eb3d8b21210bfbc6e437bc11f956d8c5a7b95b6d
mateosixtos <mateo@nlightntech.com>
2018-09-06 23:02:02 -0700

    added content to ContactInfo component

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -3,9 +3,22 @@ import React from 'react'
 
 class ContactInfo extends React.Component{
   render(){
+    const contactList = ['LinkedIn: Logo', 'GitHub: Logo', 'Twitter: Logo', 'Email: email', 'Phone: (###) ###-####' ];
     return (
       <div className="contact-info">
-        <p>"Contact Info"</p>
+        <div className="contact-list">
+          {contactList.map( (contact, index)=>{
+            return(
+              <li key={index} className={"contact-" + contact}>
+
+                <a
+                /*user clicks room triggers app's subscribeToRoom base on the room id that was click inverse work flow*/
+                  onClick={() => window.alert(contact + " "+ index)}>
+                  {contact}
+                </a>
+              </li>)
+          })}
+        </div>
       </div>
     )
   }
