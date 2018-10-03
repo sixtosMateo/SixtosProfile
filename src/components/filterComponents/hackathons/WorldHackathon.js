86ef7529f33bffe7f6c175b62b7a7b17ef980c07
mateosixtos <mateo@nlightntech.com>
2018-10-02 23:58:40 -0700

    css for the rest of hackathons componenets

--- src/components/filterComponents/hackathons/WorldHackathon.js
+++ src/components/filterComponents/hackathons/WorldHackathon.js
@@ -14,13 +14,18 @@ class WorldHackathon extends React.Component{
   render(){
     return (
       <div className="world-hackathon">
+        <div className="hackathon-title">
           <ProjectName projectName={this.props.hackathonObj.projectName}/>
           <Date date={this.props.hackathonObj.date}/>
-          <Description description={this.props.hackathonObj.description}/>
-          <Image image={this.props.hackathonObj.image}/>
-          <Learning learning={this.props.hackathonObj.learning}/>
+        </div>
+        <div className="hackathon-center">
           <Teammates teammates={this.props.hackathonObj.teammates}/>
+          <Image image={this.props.hackathonObj.image}/>
           <Tools tools={this.props.hackathonObj.tools}/>
+          <Description description={this.props.hackathonObj.description}/>
+        </div>
+
+        <Learning learning={this.props.hackathonObj.learning}/>
        </div>
 
     )
